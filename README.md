bf337fbe2f215244f6ff9e117a869a75a1e02c0e
Aleksandr Dronov <dronovspb@gmail.com>
2023-04-06 22:10:48 +0300

    readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Личный кабинет пользователя.
+
+Создано по заданию: <a  href="./task/task.pdf" target="_blank">ссылка</a>
+Фигма: <a  href="./task/form.fig" target="_blank">ссылка</a>
+
+## Используемые технологии и решения
+- Typescript
+- React
+
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,34 @@
-import { FC, useState } from "react";
+import { ChangeEvent, FC, useRef, useState } from "react";
 import styles from "./header.module.css";
 
 const Header: FC = () => {
-  const [addClass, setAddClass] = useState('')
-  const hadleClick = () => {
-    setAddClass(styles.tooltiptext_visible);
+  const [addClass, setAddClass] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const [value, setValue] = useState('Прежде чем действовать, надо понять');
+
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    if(isOpen) {
+      setIsOpen(false);
+      setAddClass('');
+    } else {
+      setAddClass(styles.tooltiptext_visible);
+      setIsOpen(true);
+      setTimeout(() => {
+        inputRef.current?.focus()
+      }, 0);
+    };
   }
+
+  const handleCange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setValue(value);
+  }
+
+  const buttonName = isOpen ? 'Сохранить' : 'Сменить статус';
+
   return (
     <div className={styles.header}>
       <h1 className={styles.title}>
@@ -13,12 +36,12 @@ const Header: FC = () => {
         <span className={styles.tooltip}>
           {" "}
           Человек №3596941
-          <div className={`${styles.tooltiptext} ${addClass}`}>
-            <input type="text" defaultValue='Прежде чем действовать, надо понять'/>
+          <div className={`${styles.tooltiptext} ${addClass}`} >
+            <input className={styles.input} ref={inputRef} type="text" value={value} onChange={handleCange}/>
           </div>
         </span>
       </h1>
-      <p className={styles.subtitle} onClick={hadleClick}>Сменить статус</p>
+      <p className={styles.subtitle} onClick={handleClick}>{buttonName}</p>
     </div> 
   );
 };
